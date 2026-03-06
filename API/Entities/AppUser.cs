b73a859f41bf82cef2afcb03ce2ccb824a16f33d
danielbarberoarroyo-cmd <danielbarberoarroyo@gmail.com>
2026-03-06 14:54:21 +0100

    Final de la seccion 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+namespace API.Entities;
+
+public class AppUser
+{
+    public String Id { get; set; } = Guid.NewGuid().ToString();
+
+    public required String DisplayName { get; set; }
+
+    public required String Email { get; set; }
+    
+}
